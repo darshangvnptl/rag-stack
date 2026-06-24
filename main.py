8d2a475608501a61a0598b6cef5c1531bf0c15c5
darshangvnptl <182540234+darshangvnptl@users.noreply.github.com>
2026-06-24 20:51:27 +0100

    setting up the environment

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_core import __version__  as core_version
+from langgraph import __version__ as graph_version
+from langchain_ollama import __version__ as ollama_version
+
 def main():
     print("Hello from rag-stack!")
-
+    print(f"langchain-core version: {core_version}")
+    print(f"langgraph version: {graph_version}")
+    print(f"langchain-ollama version: {ollama_version}")
 
 if __name__ == "__main__":
     main()
