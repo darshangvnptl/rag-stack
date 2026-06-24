90344e27afc5ac12ed62f6bd29d8f2b77865c5d7
darshangvnptl <182540234+darshangvnptl@users.noreply.github.com>
2026-06-24 21:19:42 +0100

    fixing langgraph new version call style

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+import importlib.metadata
 from langchain_core import __version__  as core_version
-from langgraph import __version__ as graph_version
+#from langgraph import __version__ as graph_version
 from langchain_ollama import __version__ as ollama_version
 
 def main():
     print("Hello from rag-stack!")
     print(f"langchain-core version: {core_version}")
-    print(f"langgraph version: {graph_version}")
+    graph_version = importlib.metadata.version("langgraph")
+    print(f"LangGraph version: {graph_version}")
     print(f"langchain-ollama version: {ollama_version}")
 
 if __name__ == "__main__":
