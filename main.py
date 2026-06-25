e7af9c2237b11a44740b287bb548cffa833c9d52
darshangvnptl <182540234+darshangvnptl@users.noreply.github.com>
2026-06-25 21:31:43 +0100

    Setup complete

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import importlib.metadata
 from langchain_core import __version__  as core_version
 #from langgraph import __version__ as graph_version
 from langchain_ollama import __version__ as ollama_version
+from langchain_ollama import ChatOllama as ollama
 
 def main():
     print("Hello from rag-stack!")
@@ -12,6 +13,11 @@ def main():
     graph_version = importlib.metadata.version("langgraph")
     print(f"LangGraph version: {graph_version}")
     print(f"langchain-ollama version: {ollama_version}")
+
+    llm = ollama(model="llama3.2", base_url="http://192.168.1.94:11434")
+    response = llm.invoke("Hello from RAG Stack!")
+    print(f"Response from Ollama: {response}")
+    print ("Setup complete. You can now run the RAG Stack application.")
 
 if __name__ == "__main__":
     main()
